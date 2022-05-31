427e8e07ed9b853e0934b45fe4c25088e3bfa256
Shabbir <developers.shabbir@arsenaltech.com>
2022-05-31 11:08:22 +0530

    e-commerce

---
+++ database/migrations/2022_05_19_044627_create_order_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_products', function (Blueprint $table) {
+            $table->id();
+            $table->integer('prd_id');
+            $table->integer('userid');
+            $table->string('prd_name');
+            $table->string('prd_price');
+            $table->string('GST');
+            $table->string('total_price');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_products');
+    }
+}
